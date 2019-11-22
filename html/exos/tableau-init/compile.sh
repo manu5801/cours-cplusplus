4170cbfff31da4d6a2ba8598dd527b87308a6df7
Emmanuel Courcelle <emmanuel.courcelle@inp-toulouse.fr>
2019-11-22 14:55:13 +0100

    Suppression de quelques vieux fichiers

--- html/exos/tableau-init/compile.sh
+++
@@ -1,9 +0,0 @@
-# Tableau avec liste d'initialisation
-# plante !
-echo 'ca ne va pas marcher'
-c++ -o tableau *.cpp
-
-# marche !
-echo 'et la ca compile'
-c++ -std=c++11 -o tableau *.cpp
-
--- html/exos/tableau-rvo/compile.sh
+++
@@ -1,7 +0,0 @@
-# Désactiver optimisation RVO
-c++ -o tableau-lent -fno-elide-constructors *.cpp
-
-# Normal
-c++ -o tableau-rapide  *.cpp
-
-
